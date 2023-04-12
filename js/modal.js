6b812a9f3eae482b33222acc2189da510055c708
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-12 13:48:11 -0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -2,16 +2,23 @@ var modal = document.getElementById("modal");
 var btn = document.getElementById("open-form");
 var span = document.getElementsByClassName("close")[0];
 
+var b = document.getElementById("b");
+
+
+
 btn.onclick = function() {
   modal.style.display = "block";
+  b.style.overflowY = 'hidden';
 }
 
 span.onclick = function() {
   modal.style.display = "none";
+  b.style.overflowY = 'auto';
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    b.style.overflowY = 'auto';
   }
 }
