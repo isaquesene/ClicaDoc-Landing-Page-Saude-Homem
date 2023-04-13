de8e8eecb9a3431d4bcac3adb20ba2ab6528a918
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-13 10:07:22 -0300

    comit

--- js/modal.js
+++ js/modal.js
@@ -8,22 +8,6 @@ btn.forEach((button) => {
   btn.addEventListener('click', () => {
     const modal = document.getElementById('open-modal');
     modal.style.display = 'block';
+    b.style.overflowY = 'hidden';
   });
 });
-
-btn.onclick = function() {
-  modal.style.display = "block";
-  b.style.overflowY = 'hidden';
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-  b.style.overflowY = 'auto';
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    b.style.overflowY = 'auto';
-  }
-}
