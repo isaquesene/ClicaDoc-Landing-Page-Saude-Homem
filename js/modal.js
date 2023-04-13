f89583571aa3752880c934c8896e0c396d91aadb
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-13 09:47:09 -0300

    comit

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 var modal = document.getElementById("modal");
-var btn = document.getElementById("open-form");
+var btns = document.getElementsByClassName("open-form");
 var span = document.getElementsByClassName("close")[0];
 
 var b = document.getElementById("b");
