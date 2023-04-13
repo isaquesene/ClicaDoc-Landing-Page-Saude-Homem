92e8aa83badce1d5e44f517ca6b5dc8d25fef5fc
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-13 09:26:36 -0300

    comit

--- js/modal2.js
+++ js/modal2.js
@@ -1,23 +1,23 @@
 var modal2 = document.getElementById("modal2");
-var btn = document.getElementById("open-form2");
-var span = document.getElementsByClassName("close")[0];
+var btn2 = document.getElementById("open-form2");
+var span2 = document.getElementsByClassName("close2")[0];
 
 var b = document.getElementById("b");
 
 
 
-btn.onclick = function() {
+btn2.onclick = function() {
   modal2.style.display = "block";
   b.style.overflowY = 'hidden';
 }
 
-span.onclick = function() {
+span2.onclick = function() {
   modal2.style.display = "none";
   b.style.overflowY = 'auto';
 }
 
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal2) {
     modal2.style.display = "none";
     b.style.overflowY = 'auto';
   }
