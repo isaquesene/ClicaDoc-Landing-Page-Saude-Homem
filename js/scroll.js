c5f051318855b706a71fcdf9d1692be22ad52b01
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-15 21:54:29 -0300

    comit

--- js/scroll.js
+++ js/scroll.js
@@ -7,4 +7,7 @@ const sr = ScrollReveal({
 sr.reveal('.home-text',{delay: 300, origin:'left'})
 sr.reveal('.home-img',{delay: 300, origin:'right'})
 
+sr.reveal('.prev-text',{delay: 300, origin:'left'})
+sr.reveal('.prev-img',{delay: 300, origin:'right'})
+
 sr.reveal('.sub-saude, .especial',{delay: 100, origin:'bottom'})
