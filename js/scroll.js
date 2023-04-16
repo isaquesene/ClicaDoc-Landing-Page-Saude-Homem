d0f3eb54058db116deafdd3549afc9834b64dbb7
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-15 22:21:02 -0300

    comit

--- js/scroll.js
+++ js/scroll.js
@@ -11,4 +11,7 @@ sr.reveal('.prev-title',{delay: 300, origin:'left'})
 sr.reveal('.prev-text',{delay: 300, origin:'left'})
 sr.reveal('.prev-img',{delay: 300, origin:'right'})
 
+sr.reveal('.items',{delay: 300, origin:'left'})
+sr.reveal('.receita-img',{delay: 300, origin:'right'})
+
 sr.reveal('.sub-saude, .especial',{delay: 100, origin:'bottom'})
