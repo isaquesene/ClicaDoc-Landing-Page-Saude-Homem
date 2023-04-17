bca00ff0e472188c80087da762f46e85c41d3a8f
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-04-16 22:09:03 -0300

    comit

--- js/scroll.js
+++ js/scroll.js
@@ -7,11 +7,11 @@ const sr = ScrollReveal({
 sr.reveal('.home-text',{delay: 300, origin:'left'})
 sr.reveal('.home-img',{delay: 300, origin:'right'})
 
-sr.reveal('.prev-title',{delay: 300, origin:'left'})
-sr.reveal('.prev-text',{delay: 300, origin:'left'})
-sr.reveal('.prev-img',{delay: 300, origin:'right'})
+//sr.reveal('.prev-title',{delay: 300, origin:'left'})
+//sr.reveal('.prev-text',{delay: 300, origin:'left'})
+//sr.reveal('.prev-img',{delay: 300, origin:'right'})
 
-sr.reveal('.items',{delay: 300, origin:'left'})
-sr.reveal('.receita-img',{delay: 300, origin:'right'})
+//sr.reveal('.items',{delay: 300, origin:'left'})
+//sr.reveal('.receita-img',{delay: 300, origin:'right'})
 
 sr.reveal('.sub-saude, .especial , .receitas',{delay: 100, origin:'bottom'})
